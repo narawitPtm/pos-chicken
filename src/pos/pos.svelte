cd725d9a309bda0cbf19952e79ba601a946e45f5
Nitikon Wichiansawang <nitikon.wc@gmail.com>
2021-10-20 14:52:00 +0700

    update menu

--- src/pos/pos.svelte
+++ src/pos/pos.svelte
@@ -1,31 +1,46 @@
 <script lang="ts">
     let src = '/picture/chicken.jpg';
+	let menuchicken: {name:string,isActive:boolean, path:string}[] = [
+		{ name: 'ทั้งหมด',  isActive: true, path:""},
+		{ name: 'โปรโมชั่น', isActive: false, path:"" },
+		{ name: 'ไก่ทอด', isActive: false, path:"" },
+		{ name: 'ข้าวเหนียว', isActive: false, path:"" },
+		{ name: 'หอมเจียว', isActive: false, path:"" }
+	];
+
+	function submenu(index: number) {
+		menuchicken.forEach((item, indexs) => {
+			if(index === indexs) {
+				item.isActive = true
+			}
+			else {
+				item.isActive = false
+			}
+		})
+		menuchicken = menuchicken
+	}
 
 </script>
-<!-- <button> -->
-<div>
-	<button class = 'Allbutton' on:click> ทั้งหมด </button>
-	<button class = 'promotionbutton' on:click> โปรโมชั่น </button>
-	<button class = 'chickenbutton' on:click> ไก่ทอด </button>
-	<button class = 'Kaobutton' on:click> ข้าวเหนียว </button>
-	<button class = 'hombutton' on:click> หอมเจียว </button>
-</div>
+	<main>
+		<div class="filter-btn-box">
+			{#each menuchicken as menu, index }	
+		  		<button class = {`filter-button ${menu.isActive ? 'active' : ''}`} on:click={() => submenu(index)}> {menu.name} </button>
+			{/each}
+		</div>
+	</main>
 
 <!-- picture -->
 <div id="nongKai">
 	<div class="card-menu">
-		<img class = "pchicken" src="{src}" alt="">
+		<img class = "chicken-img" src="{src}" alt="">
 		<div class="card-footer">
-			<div class="nongKaitidsapork">
+			<div class="chicken-text">
 				น่องไก่ติดสะโพก
 			</div>
 			<div class="Two">0</div>
 		</div>
 	</div>
 </div>	
-
-
-
 
 <style lang="scss">
    @import './pos.scss';
